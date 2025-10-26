9f45538439a41b06f21721b56d0a476fda269ba0
Jamilqadri <jamilqadri6710@gmail.com>
2025-10-26 07:10:32 +0530

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,42 +1,57 @@
 // questions.js
-// NOTE: You can add as many questions as you like here. 
-// The system will randomly select 5 from this list.
+// NOTE: Make sure the list of questions ends without a comma after the last entry.
+// The system will randomly select 5 questions from this list.
 
 const quizQuestions = [
     {
         question: "What is the first month of the Islamic calendar?",
         options: ["Muharram", "Ramadan", "Shawwal", "Dhul-Hijjah"],
-        correct: 0 // Index of the correct option (0, 1, 2, or 3)
+        correct: 0 // Muharram
     },
     {
         question: "How many obligatory daily prayers (Fard Salah) are there in Islam?",
         options: ["3", "4", "5", "6"],
-        correct: 2
+        correct: 2 // 5
     },
     {
         question: "Which prayer is performed immediately after sunset?",
         options: ["Fajr", "Dhuhr", "Asr", "Maghrib"],
-        correct: 3
+        correct: 3 // Maghrib
     },
     {
         question: "What is the name of the angel who delivers revelations to the Prophets?",
         options: ["Israfeel", "Mikaeel", "Jibreel", "Azrael"],
-        correct: 2
+        correct: 2 // Jibreel (A.S)
     },
     {
         question: "How many times is it compulsory to circle the Kaaba (Tawaf) during Hajj or Umrah?",
         options: ["3", "5", "7", "10"],
-        correct: 2
+        correct: 2 // 7 times
     },
     {
         question: "What is the name of the final Prophet in Islam?",
         options: ["Isa (A.S)", "Musa (A.S)", "Muhammad (S.A.W)", "Ibrahim (A.S)"],
-        correct: 2
+        correct: 2 // Muhammad (S.A.W)
     },
     {
         question: "What is the compulsory charity given during the month of Ramadan?",
         options: ["Zakat al-Mal", "Zakat al-Fitr", "Sadaqah", "Kaffarah"],
-        correct: 1
+        correct: 1 // Zakat al-Fitr
+    },
+    {
+        question: "Which is the longest Surah (Chapter) in the Holy Quran?",
+        options: ["Al-Fatiha", "Yaseen", "Al-Baqarah", "Al-Mulk"],
+        correct: 2 // Al-Baqarah
+    },
+    {
+        question: "What is the direction towards which Muslims turn during prayer?",
+        options: ["Masjid Al-Aqsa", "Madinah", "Jerusalem", "Kaaba"],
+        correct: 3 // Kaaba
+    },
+    {
+        question: "What is the number of verses in the Holy Quran?",
+        options: ["6666", "6236", "6500", "6348"],
+        correct: 1 // 6236 (Standard count, slight variation accepted)
     }
-    // You can add 193 more questions here (total 200)
+    // Note: No comma (,) after the last curly brace!
 ];
